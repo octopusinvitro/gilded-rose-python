20291fa235baaeef349fad42dd84bc3ebd65d071
private <user@example.com>
2023-08-28 13:14:25 +0100

    Extract Aged Brie seam

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -2,8 +2,20 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_brie(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.quality >= 50:
+            return
+
+        item.quality = item.quality + 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality = item.quality + 1
+
     def update_quality(self):
         for item in self.items:
+            if item.name == "Aged Brie":
+                return self.update_brie(item)
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -28,9 +40,6 @@ class GildedRose(object):
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
 
 class Item:
--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -19,5 +19,41 @@ class TestGildedRoseUpdateQuality(unittest.TestCase):
         verify_all_combinations(self.update_quality, [names, sell_ins, qualities])
 
 
+class TestGildedRoseUpdateBrie(unittest.TestCase):
+    def update_brie(self, sell_in, quality):
+        items = [Item("Aged Brie", sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_brie(gilded_rose.items[0])
+        return gilded_rose.items[0]
+
+    def test_decreases_sell_in_by_one(self):
+        item = self.update_brie(0, 0)
+        self.assertEqual(item.sell_in, -1)
+
+    def test_increases_quality_by_two_for_negative_sell_in(self):
+        item = self.update_brie(-1, 0)
+        self.assertEqual(item.quality, 2)
+
+    def test_increases_quality_by_two_for_zero_sell_in(self):
+        item = self.update_brie(0, 0)
+        self.assertEqual(item.quality, 2)
+
+    def test_increases_quality_by_one_for_positive_sell_in(self):
+        item = self.update_brie(1, 0)
+        self.assertEqual(item.quality, 1)
+
+    def test_does_not_increase_quality_beyond_limit_if_lower_than_limit(self):
+        item = self.update_brie(0, 49)
+        self.assertEqual(item.quality, 50)
+
+    def test_does_not_increase_quality_beyond_limit_if_equal_to_limit(self):
+        item = self.update_brie(1, 50)
+        self.assertEqual(item.quality, 50)
+
+    def test_does_not_increase_quality_beyond_limit_if_bigger_than_limit(self):
+        item = self.update_brie(1, 60)
+        self.assertEqual(item.quality, 60)
+
+
 if __name__ == '__main__':
     unittest.main()
